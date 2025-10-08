975f78053e65d28445620551d38b497d5c8070e8
Owen Price Skelly <21372141+OwenPriceSkelly@users.noreply.github.com>
2025-10-08 16:08:42 -0500

    first pass at `submit()`

--- src/groundhog_hpc/function.py
+++ src/groundhog_hpc/function.py
@@ -1,7 +1,10 @@
 import os
+from concurrent.futures import Future
 from typing import Callable
+from uuid import UUID
 
-from groundhog_hpc.runner import script_to_callable
+from groundhog_hpc.runner import script_to_callable, script_to_submittable
+from groundhog_hpc.serialization import deserialize, serialize
 from groundhog_hpc.settings import DEFAULT_ENDPOINTS, DEFAULT_WALLTIME_SEC
 
 
@@ -16,27 +19,29 @@ class Function:
         self.script_path = os.environ.get("GROUNDHOG_SCRIPT_PATH")  # set by cli
         self.endpoint = endpoint or DEFAULT_ENDPOINTS["anvil"]
         self.walltime = walltime or DEFAULT_WALLTIME_SEC
-        self.user_endpoint_config = user_endpoint_config
+        self.default_user_endpoint_config = user_endpoint_config
 
-        self._local_func = func
-        self._remote_func = None
+        self._name = func.__qualname__
+        self._local_function = func
+        self._remote_function = None
+        self._shell_function = None
 
     def __call__(self, *args, **kwargs):
-        return self._local_func(*args, **kwargs)
+        return self._local_function(*args, **kwargs)
 
     def remote(self, *args, **kwargs):
         if not self._running_in_harness():
             raise RuntimeError(
                 "Can't invoke a remote function outside of a @hog.harness function"
             )
-        if self._remote_func is None:
+        if self._remote_function is None:
             # delay defining the remote function until we're already invoking
             # the harness to avoid "No such file or directory: '<string>'" etc.
             # also avoids redefining the shell function recursively when running
             # on the remote endpoint
-            self._remote_func = self._init_remote_func()
+            self._remote_function = self._init_remote_func()
 
-        return self._remote_func(*args, **kwargs)
+        return self._remote_function(*args, **kwargs)
 
     def _running_in_harness(self) -> bool:
         # set by @harness decorator
@@ -48,8 +53,53 @@ class Function:
 
         return script_to_callable(
             self.script_path,
-            self._local_func.__qualname__,
+            self._local_function.__qualname__,
             self.endpoint,
             self.walltime,
-            self.user_endpoint_config,
+            self.default_user_endpoint_config,
         )
+
+    def submit(
+        self, *args, endpoint=None, walltime=None, user_endpoint_config=None, **kwargs
+    ):
+        import globus_compute_sdk as gc
+
+        if not self._running_in_harness():
+            raise RuntimeError(
+                "Can't invoke a remote function outside of a @hog.harness function"
+            )
+        endpoint = endpoint or self.endpoint
+        walltime = walltime or self.walltime
+        config = self.default_user_endpoint_config.copy()
+        config.update(user_endpoint_config or {})
+
+        if self._shell_function is None:
+            if self.script_path is None:
+                raise ValueError("Could not locate source file")
+            self._shell_function = script_to_submittable(
+                self.script_path, self._name, walltime
+            )
+
+        payload = serialize((args, kwargs))
+
+        with gc.Executor(UUID(endpoint), user_endpoint_config=config) as executor:
+            future = executor.submit(self._shell_function, payload)
+            return _create_deserializing_future(future)
+
+
+def _create_deserializing_future(original_future: Future) -> Future:
+    """Returns a new future that will contain the deserialized result"""
+    deserialized_future = type(original_future)()
+    if hasattr(original_future, "task_id"):
+        deserialized_future.task_id = getattr(original_future, "task_id")
+
+    def callback(fut):
+        try:
+            serialized_data = fut.result()
+            deserialized_data = deserialize(serialized_data)
+            deserialized_future.set_result(deserialized_data)
+        except Exception as e:
+            deserialized_future.set_exception(e)
+
+    original_future.add_done_callback(callback)
+    return deserialized_future
