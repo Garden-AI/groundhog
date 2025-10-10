e43f9e4aa0a751e7cf761113aed275cb2198635a
Owen Price Skelly <21372141+OwenPriceSkelly@users.noreply.github.com>
2025-10-10 10:57:02 -0500

    refactor: implement GroundhogFuture class
    ... to encapsulate auto-deserialization callback logic and original
    shell result from the ComputeFuture

--- src/groundhog_hpc/runner.py
+++ src/groundhog_hpc/runner.py
@@ -1,11 +1,9 @@
 import os
 import warnings
-from concurrent.futures import Future
-from typing import TYPE_CHECKING, Any, TypeVar
+from typing import TYPE_CHECKING, TypeVar
 from uuid import UUID
 
-from groundhog_hpc.errors import RemoteExecutionError
-from groundhog_hpc.serialization import deserialize
+from groundhog_hpc.future import GroundhogFuture
 from groundhog_hpc.templating import template_shell_command
 
 warnings.filterwarnings(
@@ -18,10 +16,8 @@ if TYPE_CHECKING:
     import globus_compute_sdk
 
     ShellFunction = globus_compute_sdk.ShellFunction
-    ShellResult = globus_compute_sdk.ShellResult
 else:
     ShellFunction = TypeVar("ShellFunction")
-    ShellResult = TypeVar("ShellResult")
 
 if os.environ.get("PYTEST_VERSION") is not None:
     # we lazy import globus compute everywhere to avoid possible
@@ -65,43 +61,10 @@ def submit_to_executor(
     user_endpoint_config: dict,
     shell_function: ShellFunction,
     payload: str,
-) -> Future:
+) -> GroundhogFuture:
     import globus_compute_sdk as gc
 
     with gc.Executor(endpoint, user_endpoint_config=user_endpoint_config) as executor:
         future = executor.submit(shell_function, payload=payload)
-        deserializing_future = _create_deserializing_future(future)
+        deserializing_future = GroundhogFuture(future)
         return deserializing_future
-
-
-def _create_deserializing_future(original_future: Future) -> Future:
-    """Returns a new future that will contain the deserialized result"""
-    deserialized_future = type(original_future)()
-
-    def callback(fut):
-        try:
-            serialized_result = fut.result()
-            deserialized_result = _process_shell_result(serialized_result)
-            deserialized_future.set_result(deserialized_result)
-
-        except Exception as e:
-            deserialized_future.set_exception(e)
-
-        finally:
-            if hasattr(fut, "task_id"):
-                deserialized_future.task_id = fut.task_id  # ty: ignore[unresolved-attribute]
-
-    original_future.add_done_callback(callback)
-    return deserialized_future
-
-
-def _process_shell_result(shell_result: ShellResult) -> Any:
-    if shell_result.returncode != 0:
-        msg = f"Remote execution failed with exit code {shell_result.returncode}"
-        raise RemoteExecutionError(
-            message=msg,
-            stderr=shell_result.stderr,
-            returncode=shell_result.returncode,
-        )
-
-    return deserialize(shell_result.stdout)
