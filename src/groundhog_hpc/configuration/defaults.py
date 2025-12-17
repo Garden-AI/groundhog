ea5bc6bf22baac7ee73967a8dfee0d4b0c2c6c69
Owen Price Skelly <21372141+OwenPriceSkelly@users.noreply.github.com>
2025-12-17 13:55:52 -0600

    feat: `endpoint_setup` handled like `worker_init` (#114)

--- src/groundhog_hpc/configuration/defaults.py
+++ src/groundhog_hpc/configuration/defaults.py
@@ -1,6 +1,7 @@
 # Default Globus Compute Executor configuration
 DEFAULT_USER_CONFIG = {
     "worker_init": "",
+    "endpoint_setup": "",
 }
 
 # default maximum execution time for remote functions (in seconds)
